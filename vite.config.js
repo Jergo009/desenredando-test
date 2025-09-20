86f5371b9a8ab328774329511bbae6a430951125
Jergo009 <jramirezg3@miumg.edu.gt>
2025-09-20 07:55:02 -0600

    change initial route

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/desenredando-test",
+  //base: "/desenredando-test",
+  base: "/",
   plugins: [react()],
 });
