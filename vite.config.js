a73f83b7824bce4df7a6aac14685a905f587d669
Jergo009 <jramirezg3@miumg.edu.gt>
2025-09-20 08:14:13 -0600

    unchanges on vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  //base: "/desenredando-test",
-  base: "/",
+  base: "/desenredando-test",
   plugins: [react()],
 });
